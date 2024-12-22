295c89fbde13f4248842923f3aef37c068a596ae
Artur Lissin <artdotlis@pm.me>
2024-12-22 23:13:43 +0100

    feat: remove radon

--- bin/deploy/post.sh
+++ bin/deploy/post.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-PYT="$(pwd)/.venv/bin"
-LOC="$HOME/.local/bin"
-
-echo "installing python-radon"
-if "$PYT"/python -m pip install "radon>=5.1,<6"; then
-    echo "radon successfully installed"
-else
-    echo "radon already installed"
-fi
-
-mkdir -p "$LOC"
-
-if ln -s "$PYT"/radon "$LOC/radon"; then
-    echo "link created"
-fi
+echo "nothing to do!"
