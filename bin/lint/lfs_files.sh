b0d46de4d8c8952c254010e83a0b07bcbca3e5b2
Artur Lissin <artdotlis@pm.me>
2026-03-28 01:02:57 +0100

    feat: move to new structure

---
+++ bin/lint/lfs_files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+FILES=()
+
+filter_and_collect() {
+    local input_file
+    while IFS= read -r input_file; do
+        [[ -z "$input_file" ]] && continue
+        if [[ -e "$input_file" ]]; then
+            FILES+=("$input_file")
+        fi
+    done
+}
+
+if [ "$#" -gt 0 ]; then
+    filter_and_collect < <(printf "%s\n" "$@")
+else
+    filter_and_collect < <(git ls-files)
+fi
+
+for file in "${FILES[@]}"; do
+  if git lfs ls-files | grep -q "$file"; then
+    status=$(git status --porcelain "$file")
+    if [[ "$status" =~ ^D || -z "$status" ]]; then
+      continue
+    fi
+    size=$(stat --format=%s "$file")
+    if [ "$size" -gt 512000 ]; then
+      echo "LFS file $file is larger than 500 KB ($size bytes). Please reduce its size."
+      exit 1
+    fi
+  fi
+done
---
+++ bin/lint/licenses.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+set -euo pipefail
+ROOT="$(dirname "$(realpath "$0")")/../.."
+source "$ROOT/package.env"
+
+find "$ROOT" -type f -name "*.license" | while read -r license; do
+  original="${license%.license}"
+
+  if [[ ! -e "$original" ]]; then
+    echo "Removing orphan license: $license"
+    rm "$license"
+  fi
+done
+
+SOFTWARE_LIC="MIT"
+DATA_LIC="CC-BY-4.0"
+PUB_LIC="CC0-1.0"
+YEAR=$(date +%Y)
+
+if [[ -n "$COPYRIGHT" ]]; then
+    echo "COPYRIGHT is set: $COPYRIGHT"
+
+    LICENSE_FILES=("$ROOT/LICENSE" "$ROOT/LICENSES/$SOFTWARE_LIC.txt")
+
+    for license_file in "${LICENSE_FILES[@]}"; do
+        if [[ ! -f "$license_file" ]]; then
+            echo "License file $license_file does not exist"
+            exit 1 
+        fi
+        if ! grep -q "$YEAR" "$license_file"; then
+            echo "License file $license_file does not exist or COPYRIGHT not found"
+            exit 1 
+        fi
+        if ! grep -q "$YEAR" "$license_file"; then
+            echo "Current year ($YEAR) not be found in $license_file"
+            exit 1
+        fi
+        if ! grep -q "$SOFTWARE_LIC" "$license_file"; then
+            echo "License ($SOFTWARE_LIC) not be found in $license_file"
+            exit 1
+        fi
+    done
+else
+    echo "COPYRIGHT is not set or is empty"
+    exit 1
+fi
+
+# For python projects
+if ! grep -q "$SOFTWARE_LIC" "$ROOT/pyproject.toml"; then
+    echo "License ($SOFTWARE_LIC) not be found in pyproject.toml"
+    exit 1
+fi
+
+FILES=()
+
+filter_and_collect() {
+    local input_file
+    while IFS= read -r input_file; do
+        [[ -z "$input_file" ]] && continue
+        [[ "$input_file" == LICENSES/* ]] && continue
+        [[ "$input_file" == "bin/lint/licenses.sh" ]] && continue
+        if [[ -e "$input_file" ]]; then
+            FILES+=("$input_file")
+        fi
+    done
+}
+
+if [ "$#" -gt 0 ]; then
+    filter_and_collect < <(printf "%s\n" "$@")
+else
+    filter_and_collect < <(git ls-files)
+fi
+
+
+SOFTWARE=("py" "sh" "Dockerfile" "conf" "template")
+CC_BY=("jpg" "png" "ico" "webp" "avif" "md" "yaml" "yml" "json" "toml" "txt")
+CC0_FILES=(".gitignore" ".gitattributes" ".env" "package.env" "bun.lock" ".dockerignore")
+MIT_FILES=("Makefile" "shellcheckrc")
+MIT_FOLDERS=()
+
+mit_to_annotate=()
+ccby_to_annotate=()
+cc0_to_annotate=()
+
+check_folder_in_array() {
+    local search="$1"
+    shift
+    local -a container=("$@")
+
+    for ele in "${container[@]}"; do
+        if [[ "$search" == *"$ele"* ]]; then
+            return 0 
+        fi
+    done
+
+    return 1  
+}
+
+for file in "${FILES[@]}"; do
+    extension="${file##*.}"    
+    file_name=$(basename "$file")
+    file_dir=$(dirname "$file")
+    if check_folder_in_array "$file_dir" "${MIT_FOLDERS[@]}"; then
+        mit_to_annotate+=("$file")
+        continue
+    fi
+    if check_folder_in_array "$extension" "${SOFTWARE[@]}"; then
+        mit_to_annotate+=("$file")
+        continue
+    fi
+    if check_folder_in_array "$extension" "${CC_BY[@]}"; then
+        ccby_to_annotate+=("$file")
+        continue
+    fi
+    if check_folder_in_array "$file_name" "${MIT_FILES[@]}"; then
+        mit_to_annotate+=("$file")
+        continue
+    fi
+    if check_folder_in_array " ${file_name}" "${CC0_FILES[@]}"; then
+        cc0_to_annotate+=("$file")
+        continue
+    fi
+done
+
+if [ ${#mit_to_annotate[@]} -gt 0 ]; then
+    echo "annotating MIT"
+    make runUV reuse annotate -c "$COPYRIGHT" -l "$SOFTWARE_LIC" -y "$YEAR" --merge-copyrights --fallback-dot-license "${mit_to_annotate[@]}"
+else
+    echo "No MIT files to annotate"
+fi
+
+if [ ${#ccby_to_annotate[@]} -gt 0 ]; then
+    echo "annotating CC-BY"
+    make runUV reuse annotate -c "$COPYRIGHT" -l "$DATA_LIC" -y "$YEAR" --merge-copyrights --fallback-dot-license "${ccby_to_annotate[@]}"
+else
+    echo "No CC-BY files to annotate"
+fi
+
+if [ ${#cc0_to_annotate[@]} -gt 0 ]; then
+    echo "annotating CC0"
+    make runUV reuse annotate -c "$COPYRIGHT" -l "$PUB_LIC" -y "$YEAR" --merge-copyrights --fallback-dot-license "${cc0_to_annotate[@]}"
+else
+    echo "No CC0 files to annotate"
+fi
+
+if ! make runUV reuse lint; then
+    echo "Linting failed!"
+    exit 1
+fi
+
+if ! git diff --quiet || [ -n "$(git ls-files --other --exclude-standard)" ]; then
+    echo "There are uncommitted changes or untracked files in the repository."
+    git ls-files --other --exclude-standard
+    git --no-pager diff
+    exit 1
+fi
